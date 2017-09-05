ee17707ae27dc792c91da16f172615dc5fb90b3d
DESKTOP-KLSCFEA\Fabricia <fabriciasnes@gmail.com>
2017-09-05 14:31:07 -0300

    Microservices Standard Access Control - Gateway creation

---
+++ microservices-standard-access-control/gateway/src/main/java/com/unifacisa/standard/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package com.unifacisa.standard.repository;
+
+import com.unifacisa.standard.domain.Authority;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+/**
+ * Spring Data MongoDB repository for the Authority entity.
+ */
+public interface AuthorityRepository extends MongoRepository<Authority, String> {
+}
---
+++ microservices-standard-access-control/gateway/src/main/java/com/unifacisa/standard/service/dto/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Data Transfer Objects.
+ */
+package com.unifacisa.standard.service.dto;
